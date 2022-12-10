7e98849f274e860767d4f33c62e91f6e8bdb23b4
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2022-12-10 16:03:08 +0900

    MVC 패턴 추가.
    유튜브 홍팍 강의
    https://www.youtube.com/watch?v=5vfxeRJJsWo&list=PLyebPLlVYXCiYdYaWRKgCqvnCFrLEANXt&index=6

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 이 Class가 Controller라는 것을 표시해줌.
+public class FirstController {
+    @GetMapping("/hi") // main 주소의 /hi 영역에 아래 method를 매핑함.
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "배준범");
+        return "greetings"; // throw to browser templates/greeting.mustache
+    }
+}
