9443ba4197547599ee9ee6df3c8c0da65acf4247
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2022-12-14 22:24:09 +0900

    Lombok 추가 및 logging 적용
    Youtube 홍팍
    https://www.youtube.com/watch?v=2VYBQ_99RJg

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // Logging Annotation
 public class ArticleController {
     @Autowired // Springboot가 미리 생성해놓은 객체를 자동으로 연결해줌.
     private ArticleRepository articleRepository;
@@ -20,15 +22,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create") // form의 action과 같은 주소
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        // System.out.println(form.toString()); => logging
+        log.info(form.toString());
 
         // Convert DTO to Entity
         Article article = form.toEntity(); // Convert form's contents to Article Entity
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString());
 
         // throw Entity to Repository and save to DB
         Article saved = articleRepository.save(article); // Return saved entity.
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,36 +1,24 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // Entity라고 명시해야 DB가 인식할 수 있음.
+@AllArgsConstructor
+@ToString
 public class Article {
-
     @Id // 대표값 지정
     @GeneratedValue // 자동 생성 Anotation.
     private  Long id;
-
 
     @Column // Field를 Column에 mapping
     private String title;
 
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
