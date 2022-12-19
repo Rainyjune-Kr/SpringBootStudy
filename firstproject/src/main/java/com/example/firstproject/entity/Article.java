0d88d3583f7743d1d7904b4ed445e77d3dcbabd8
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2022-12-19 21:37:21 +0900

    내용수정
    Youtube 홍팍
    https://www.youtube.com/watch?v=RUsTFA5GCiM

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // Entity라고 명시해야 DB가 인식할 수 있음.
 @AllArgsConstructor
@@ -17,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 public class Article {
     @Id // 대표값 지정
-    @GeneratedValue // 자동 생성 Anotation.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 Anotation.strategy를 넣어 DB에서 알아서 가져오도록 적용.
     private  Long id;
 
     @Column // Field를 Column에 mapping
